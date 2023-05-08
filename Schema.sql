aec792fc19d5ba9849bfab0676529ab345fdf7fb
AriniainaRak <99273758+AriniainaRak@users.noreply.github.com>
2023-05-09 00:03:30 +0300

    examen

---
+++ Schema.sql
@@ -0,0 +1,29 @@
+create role web with password 'web';
+
+create database web;
+
+\c web
+
+create table utilisateur (
+    idutilisateur serial primary key,
+    nom varchar(50),
+    motdepasse varchar(100),
+    acces int
+);
+insert into utilisateur(nom,motdepasse,acces) values('admin','123456',1);
+insert into utilisateur(nom,motdepasse,acces) values('utilisateur','123456',0);
+
+select * from utilisateur where acces=1 and nom='admin' and motdepasse='123456';
+
+create table info (
+    idinfo serial primary key,
+    titre varchar(50),
+    dateajout TIMESTAMP default now(),
+    contenu text,
+    statut int default 0,
+    idutilisateur int,
+    photo varchar(255),
+    foreign key (idutilisateur) references utilisateur(idutilisateur)
+);
+insert into info(titre,contenu,idutilisateur) VALUES('ChatGPT','chatGPT est le meuilleur, disent les amateurs',1);  
+insert into info(titre,contenu,idutilisateur) VALUES('INfo 2','contenu de info 2 et tres peu de blabla',1);
