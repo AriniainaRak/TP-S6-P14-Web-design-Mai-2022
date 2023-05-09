57250c1cc184a3e79bfc95a2fbbd194f2df8d4f2
AriniainaRak <99273758+AriniainaRak@users.noreply.github.com>
2023-05-09 16:14:58 +0300

    Update detailinfo.blade.php

--- resources/views/detailinfo.blade.php
+++ resources/views/detailinfo.blade.php
@@ -8,14 +8,14 @@
     @foreach ($detail as $details)
     <meta name="description" content="{{$details->resume}}">
     <title>{{$details->Titre}}</title>
-    <link rel="stylesheet" href="('assets/bootstrap/css/bootstrap.min.css')">
+    <link rel="stylesheet" href="https://hot-coil-production.up.railway.app/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
-    <link rel="stylesheet" href="('assets/fonts/font-awesome.min.css')">
-    <link rel="stylesheet" href="('assets/fonts/ionicons.min.css')">
-    <link rel="stylesheet" href="('assets/css/Contact-Form-Clean.css')">
-    <link rel="stylesheet" href="('assets/css/Footer-Basic.css')">
-    <link rel="stylesheet" href="('assets/css/Login-Form-Clean.css')">
+    <link rel="stylesheet" href="https://hot-coil-production.up.railway.app/assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="https://hot-coil-production.up.railway.app/assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="https://hot-coil-production.up.railway.app/assets/css/Contact-Form-Clean.css">
+    <link rel="stylesheet" href="https://hot-coil-production.up.railway.app/assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="https://hot-coil-production.up.railway.app/assets/css/Login-Form-Clean.css">
 </head>
 
 <body>
